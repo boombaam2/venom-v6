b0dfab620d820ed62eb78b0a3499dc73aa705c95
boombaam2 <singershivam16@gmail.com>
2024-09-29 13:05:16 +0530

    m.py

--- m.py
+++ m.py
@@ -11,7 +11,7 @@ keep_alive()
 bot = telebot.TeleBot('7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E')
 
 # Admin user IDs
-admin_id = ["5588464519"]
+admin_id = ["5511242016"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -48,7 +48,7 @@ allowed_user_ids = read_users()
 
 # Function to log command to the file
 def log_command(user_id, target, port, time):
-    admin_id = ["5588464519"]
+    admin_id = ["5511242016"]
     user_info = bot.get_chat(user_id)
     if user_info.username:
         username = "@" + user_info.username
